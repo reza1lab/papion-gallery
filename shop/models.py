c5db72f2fc7599287fc9370ca92a6e3882b5bb7b
MohammadReza <seyedreza1385@gmail.com>
2026-08-14 20:24:20 +0330

    Create product model and migrations

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.PositiveIntegerField()
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
+    category = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
